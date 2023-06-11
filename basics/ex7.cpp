80cfdc7170744afcf1dfc7066692027a373935f4
Atul <135580429+Atul-k-m@users.noreply.github.com>
2023-06-11 09:20:35 +0530

    Add files via upload

---
+++ basics/ex7.cpp
@@ -0,0 +1,11 @@
+/*Write a program that uses a for to sum the numbers from 50 to 100*/
+#include<iostream>
+int main(){
+    int sum =0;
+    for(int val=50; val<=100;++val){
+        sum += val;
+
+    }
+    std::cout<<sum;
+
+}
