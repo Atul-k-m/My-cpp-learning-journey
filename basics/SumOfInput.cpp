ca34e46a87bbf4186bd87f8a6f68bdfeb10dcbda
Atul <135580429+Atul-k-m@users.noreply.github.com>
2023-06-11 13:14:23 +0530

    Delete basics directory

--- basics/SumOfInput.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-int main(){
-    int sum = 0 , val = 0;
-    while(std::cin>>val){
-        sum += val;
-    }
-    std::cout<<sum;
-}
--- basics/ex1.cpp
+++
@@ -1,6 +0,0 @@
-//Write a program to print Hello, World on the standard output
-#include <iostream>
- int main(){
-    std::cout<<"hello world";
-    return 0;
- }
