622d9664a6d3ec1170ebbd0774b32396ece41b99
Atul <135580429+Atul-k-m@users.noreply.github.com>
2023-07-04 21:10:21 +0530

    Add files via upload

---
+++ basics/SumOfInput.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+    int sum = 0 , val = 0;
+    while(std::cin>>val){
+        sum += val;
+    }
+    std::cout<<sum;
+}
---
+++ basics/ex1.cpp
@@ -0,0 +1,6 @@
+//Write a program to print Hello, World on the standard output
+#include <iostream>
+ int main(){
+    std::cout<<"hello world";
+    return 0;
+ }
